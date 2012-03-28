78123da4d5bb23ac41fdd8e48509bd0666d2190e
Leendert <chillhamster@gmail.com>
2012-03-28 19:22:48 +0200

    Spawny branch, preparing spawning functionality

--- loadable/tinyex.c
+++ loadable/tinyex.c
@@ -1,6 +1,15 @@
 #include <svp/testoutput.h>
-int test(void)
-{
-  output_string("hello world\n", 1);
+int test(void *p, int argc, char **argv, char*env){
+
+  /*Use as argument, unrelated to argv...*/
+  if (argc > 1) {
+    output_string(argv[1],2);
+    output_char('\n',2);
+  }
+  //output_string("hello world\n", 1);
+  void (*s)(char*,int,char**,char*);
+  s = p;
+  /*Call 'me' with other args*/
+  (*s)(argv[0], argc-1, argv, env);
   return 0;
 }
